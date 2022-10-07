76018e85887d3e9b4c4440648b4252c2a806c050
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-07 14:34:23 -0300

    feat: post route

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ async def home():
     return {"message": "You're on root route, please visit /docs to see all routes or /tasks to see all tasks"}
 
 
-@app.get("/tasks", tags=["Home"])
+@app.get("/tasks", tags=["Task"])
 async def list_tasks():
     with Session(engine) as session:
         tasks = select(Task)
         return list(session.exec(tasks))
+
+@app.post("/tasks", tags=["Task"], status_code=201)
+async def add_new_task(task: Task):
+    with Session(engine) as session:
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return {"created": task}

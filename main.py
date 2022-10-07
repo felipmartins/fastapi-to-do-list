38a3b65698ff054e85424643e210ca7eadcb2cb2
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-07 14:40:16 -0300

    feat: put route

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI
 from sqlmodel import Session, select
 from db import engine
@@ -17,10 +18,26 @@ async def list_tasks():
         tasks = select(Task)
         return list(session.exec(tasks))
 
+
 @app.post("/tasks", tags=["Task"], status_code=201)
-async def add_new_task(task: Task):
+async def add_new_task(req_task: Task):
     with Session(engine) as session:
+        session.add(req_task)
+        session.commit()
+        session.refresh(req_task)
+        return {"created": req_task}
+
+
+@app.put("/tasks/{id}", tags=["Task"], status_code=200)
+async def add_new_task(id: int, req_task: Task):
+    with Session(engine) as session:
+        query = select(Task).where(Task.id == id)
+        task = session.exec(query).one()
+        task.title = req_task.title
+        task.description = req_task.description
+        task.status = req_task.status
+        task.update_date = datetime.now()
         session.add(task)
         session.commit()
         session.refresh(task)
-        return {"created": task}
+        return {"edited": task}

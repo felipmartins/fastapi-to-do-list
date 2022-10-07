65f10efb832312bf6adcc1152a41e4c20bced41a
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-07 14:28:10 -0300

    feat: test add row to task table

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, create_engine, Session
-import task_model
+from task_model import Task
 
 sqlite_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_name}"
@@ -8,3 +8,8 @@ engine = create_engine(sqlite_url, echo=True)
 
 SQLModel.metadata.create_all(engine)
 
+def test_add_task():
+    with Session(engine) as session:
+        task = Task(title='Minha task', description="Minha description")
+        session.add(task)
+        session.commit()

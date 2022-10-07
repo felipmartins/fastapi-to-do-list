58072512faddba1a36b9bcc76f285e878d246297
Felipe Martins Gomes Pereira <felipmartins94@gmail.com>
2022-10-07 14:12:33 -0300

    setup initial app

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/", tags=["Home"])
+async def home():
+    return {"message": "You're on root route, please visit /docs to see all routes or /tasks to see all tasks"}
+
+
+@app.get("/tasks", tags=["Home"])
+async def list_tasks():
+    return {"message": "All tasks"}
